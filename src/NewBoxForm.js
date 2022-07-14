46d8fce83695b5f163f4d12a357c876184f89463
tdhood <taylordannhood@gmail.com>
2022-07-14 12:06:16 -0700

    new box form

---
+++ src/NewBoxForm.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+/** Form for creating a new box 
+ * Prop:
+ *  - submitItem -- on submission, sends data to parent
+ * 
+ * Has state for backgroundColor, height, width;
+ * 
+ * BoxList -> NewBoxForm
+*/
+function NewBoxForm({ submitItem }) {
+    const initialState = {
+        backgroundColor: "",
+        height: "",
+        width: "",
+    }
+    const [formData, setFormData] = useState(initialState);
+
+    /** Send BackgroundColor, height, width to parent and clear form */
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        submitItem(formData);
+        setFormData(initialState);
+      }
+
+    /**Update form state with updated inputs */
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormData(fData => ({
+            ...fData,
+            [name]: value,
+        }));
+    }
+    /** render form */
+    return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="backgroundColor">Background Color:</label>
+      <input
+        id="backgroundColor"
+        name="backgroundColor"
+        value={formData.backgroundColor}
+        onChange={handleChange}
+      />
+      <label htmlFor="height">Height:</label>
+      <input
+        id="height"
+        name="height"
+        value={formData.height}
+        onChange={handleChange}
+      />
+      <label htmlFor="width">Width:</label>
+      <input
+        id="width"
+        name="width"
+        value={formData.width}
+        onChange={handleChange}
+      />
+    </form>
+    );
+}
+export default NewBoxForm;
